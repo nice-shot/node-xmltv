7422b4763e58d606c086a0967ce7f7b351142f78
nadavs <nadavs@fabrixsystems.com>
2015-06-04 14:35:11 +0300

    created the first basic test - now to change everything

--- test/xmltv_test.js
+++ test/xmltv_test.js
@@ -1,6 +1,19 @@
+var fs = require('fs');
 var test = require('tape');
 
+var XMLTVParser = require('../lib/xmltv.js');
+
 test('XMLTV Parsing', function (t)  {
-  t.pass('Placeholder');
-  t.end();
+  var input = fs.createReadStream(__dirname + '/eu_listings.xml');
+  var parser = new XMLTVParser();
+  input.pipe(parser);
+
+  var programmes = [];
+  var channels = [];
+
+  parser.on('end', function (){
+    t.equal(programmes.length, 87, 'Parsed all the programme tags');
+    t.equal(channels.length, 2, 'Parsed all the channel tags');
+    t.end();
+  });
 });
