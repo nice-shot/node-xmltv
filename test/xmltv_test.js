867d4415a80908466631a35ed3d0ff84d58b6291
nadavs <nadavs@fabrixsystems.com>
2015-06-04 18:09:20 +0300

    added smarter date parsing

--- test/xmltv_test.js
+++ test/xmltv_test.js
@@ -10,12 +10,21 @@ test('XMLTV Parsing', function (t)  {
 
   var programmes = [];
 
+  parser.parseDate('blabla');
   parser.on('programme', function (programme) {
     programmes.push(programme);
   });
 
   parser.on('end', function (){
     t.equal(programmes.length, 87, 'Parsed all the programme tags');
+    var firstProgramme = programmes[0];
+    t.equal(firstProgramme.channel, '3sat.de', 'Parsed channel successfully');
+    t.deepEqual(
+      firstProgramme.start,
+      new Date('2015-06-03T02:50:00+02:00'),
+      'Parsed start successfully'
+    );
     t.end();
   });
 });
+
