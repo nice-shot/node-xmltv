5c74a02448c9f56988fb5cae17dd2e18e722b3b8
nadavs <nadavs@fabrixsystems.com>
2015-06-07 09:06:22 +0300

    added title column

--- test/xmltv_test.js
+++ test/xmltv_test.js
@@ -10,7 +10,6 @@ test('XMLTV Parsing', function (t)  {
 
   var programmes = [];
 
-  parser.parseDate('blabla');
   parser.on('programme', function (programme) {
     programmes.push(programme);
   });
@@ -18,12 +17,21 @@ test('XMLTV Parsing', function (t)  {
   parser.on('end', function (){
     t.equal(programmes.length, 87, 'Parsed all the programme tags');
     var firstProgramme = programmes[0];
-    t.equal(firstProgramme.channel, '3sat.de', 'Parsed channel successfully');
-    t.deepEqual(
-      firstProgramme.start,
+    t.equal(firstProgramme.channel, '3sat.de', 'Parsed channel');
+    t.deepEqual(firstProgramme.start,
       new Date('2015-06-03T02:50:00+02:00'),
-      'Parsed start successfully'
+      'Parsed start'
     );
+    t.deepEqual(
+      firstProgramme.end,
+      new Date('2015-06-03T04:45:00+02:00'),
+      'Parsed end'
+    );
+    t.deepEqual(firstProgramme.title,
+      ['Dünkirchen, 2. Juni 1940'],
+      'Parsed title'
+    );
+
     t.end();
   });
 });
