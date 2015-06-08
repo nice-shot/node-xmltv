9127c2c48afefa486c8abc1129e06e46c5adafad
nadavs <nadavs@fabrixsystems.com>
2015-06-08 09:03:33 +0300

    coverage is not related to grunt anymore

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,39 +20,14 @@ module.exports = function(grunt) {
             test: {
                 src: ['test/**/*.js']
             }
-        },
-        // istanbul stuff
-        instrument: {
-            files: 'lib/*.js',
-            options: {
-                lazy: true,
-                basePath: 'coverage/instrument'
-            }
-        },
-        storeCoverage: {
-            options: {
-                dir: 'coverage/reports'
-            }
-        },
-        makeReport: {
-            src: 'coverage/**/*.json',
-            options: {
-                type: 'lcov',
-                dir: 'coverage/reports',
-                print: 'detail'
-            }
         }
     });
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-tape');
-    grunt.loadNpmTasks('grunt-istanbul');
 
     // Default task.
     grunt.registerTask('test', ['tape']);
     grunt.registerTask('default', ['jshint', 'test']);
-    grunt.registerTask('coverage',
-        ['instrument', 'test', 'storeCoverage', 'makeReport']
-    );
 };
